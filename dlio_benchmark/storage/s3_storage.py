babab85be8b556dad909699e9fe281b79f9913c5
Russ-Fellows <russ@evaluatorgroup.com>
2025-04-11 10:33:04 -0600

    Update s3_storage.py
    Adding in minimal code to support S3.  This uses boto3 for S3 "List" operations, and a new Python, Rust based library for I/O performance sensitive operations such as "Get" and "Put".  Note, we may amend this later but also calling our Rust library for the List operations.

--- dlio_benchmark/storage/s3_storage.py
+++ dlio_benchmark/storage/s3_storage.py
@@ -14,6 +14,22 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
+"""
+   Copyright (c) 2025, Signal65, Futurum LLC
+   All Rights Reserved
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
 from time import time
 
 from dlio_benchmark.common.constants import MODULE_STORAGE
@@ -23,9 +39,54 @@ import os
 
 from dlio_benchmark.utils.utility import Profile
 
+######
+# dlio_benchmark/storage/s3_storage.py`  was a stub: every method delegates to  `DataStorage`, which immediately returns  `None`
+# This is why it doesn't work to perform operations like Get, Put and List. 
+# For now, "List" operations are implemented as a minimal, boto3‑based implementation, since it is not performance critical
+# 
+# The new, backend Rust library will be used for the heavy data path I/O like Get and Put.
+#
+######
+# New imports
+import boto3, os, re
+from dlio_benchmark.storage.storage_handler import DataStorage, Namespace
+from dlio_benchmark.common.enumerations import NamespaceType, MetadataType
+
 dlp = Profile(MODULE_STORAGE)
 
+# New definitions 
+class S3RustStorage(DataStorage):
+    def __init__(self, bucket, framework=None):
+        super().__init__(framework)
+        self.namespace = Namespace(bucket, NamespaceType.FLAT)
+        self._s3 = boto3.client("s3")
 
+    # ---- metadata helpers used by DLIO ----
+    def get_uri(self, key):                    # s3://bucket/obj
+        return f"s3://{os.path.join(self.namespace.name, key)}"
+
+    def walk_node(self, prefix, use_pattern=False):
+        paginator = self._s3.get_paginator("list_objects_v2")
+        prefix = prefix.lstrip("/")
+        keys = []
+        for page in paginator.paginate(Bucket=self.namespace.name,
+                                       Prefix=prefix):
+            keys += [c["Key"] for c in page.get("Contents", [])]
+        if use_pattern:          # crude glob for “*/*.npz” etc.
+            regex = re.compile(os.path.basename(prefix).replace("*", ".*"))
+            keys = [k for k in keys if regex.match(os.path.basename(k))]
+        return keys
+
+    def get_node(self, key=""):
+        return MetadataType.S3_OBJECT if key else MetadataType.DIRECTORY
+
+    # create/delete/put/get can be stubs for read‑only benchmarking
+    def create_namespace(self, exist_ok=False): return True
+    def create_node(self, id, exist_ok=False):  return True
+    def delete_node(self, id):                  return True
+
+
+# Old Class definition, Not used?
 class S3Storage(DataStorage):
     """
     Storage APIs for creating files.
@@ -74,3 +135,6 @@ class S3Storage(DataStorage):
 
     def get_basename(self, id):
         return os.path.basename(id)
+
+
+
